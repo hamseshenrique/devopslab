d78940a39516010bd7ece323e9475303157807b8
hamseshenrique <hamseshenrique@gmail.com>
2022-01-23 19:42:43 -0300

    Fix CSRF

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
-app = Flask(__name__)
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
     return "Hello World Hamses Henrique - HMS SOFTWARE"
 
 if __name__ == '__main__':
-    app.run()
+    app = Flask(__name__)
+    csrf.init_app(app)
