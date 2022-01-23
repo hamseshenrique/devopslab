8f9ee9b0bd9de6dee2887798833962044f029132
hamseshenrique <hamseshenrique@gmail.com>
2022-01-23 20:01:38 -0300

    Bug Removed

--- app.py
+++ app.py
@@ -6,16 +6,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World Hamses Henrique - HMS SOFTWARE"
-    
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+    return "Hello World Hamses Henrique - HMS SOFTWARE"      
 
 if __name__ == '__main__':
     app.run()
